1a8504f2998e45aca5190c44766cc0d07b9547bc
AryanGKulkarni <95529889+AryanGKulkarni@users.noreply.github.com>
2024-09-25 16:45:35 +0530

    update

--- report.py
+++ report.py
@@ -3,9 +3,9 @@ import joblib
 from sklearn.metrics import classification_report
 import xgboost as xgb
 
-normal_data = pd.read_csv(r"C:\Users\aryan\Desktop\BTP\Dataset\normal\14.336.csv", header=None, nrows=20000)
-v_misalignment_data = pd.read_csv(r"C:\Users\aryan\Desktop\BTP\Dataset\vertical-misalignment\1.90mm\16.1792.csv", header=None, nrows=20000)
-h_misalignment_data = pd.read_csv(r"C:\Users\aryan\Desktop\BTP\Dataset\horizontal-misalignment\2.0mm\21.7088.csv", header=None, nrows=20000)
+normal_data = pd.read_csv(r"C:\Users\aryan\Desktop\BTP\Dataset\normal\14.336.csv", header=None, nrows=20)
+v_misalignment_data = pd.read_csv(r"C:\Users\aryan\Desktop\BTP\Dataset\vertical-misalignment\1.90mm\16.1792.csv", header=None, nrows=20)
+h_misalignment_data = pd.read_csv(r"C:\Users\aryan\Desktop\BTP\Dataset\horizontal-misalignment\2.0mm\21.7088.csv", header=None, nrows=20)
 
 column_names = ['tachometer_signal', 'underhang_accelerometer_axial', 'underhang_accelerometer_radial',
                 'underhang_accelerometer_tangential', 'overhang_accelerometer_axial', 'overhang_accelerometer_radial',
@@ -14,7 +14,7 @@ column_names = ['tachometer_signal', 'underhang_accelerometer_axial', 'underhang
 normal_data.columns = column_names
 h_misalignment_data.columns = column_names
 v_misalignment_data.columns = column_names
-model_path = './Models/test/xgb_model.pkl'
+model_path = './Models/main/gnb_model.pkl'
 
 normal_data['label'] = 0
 h_misalignment_data['label'] = 1
@@ -31,9 +31,9 @@ model = joblib.load(model_path)
 X_test_scaled = scaler.transform(X_test)
 print("Test Started")
 
-if(model_path.find("xgb")):
-    print("xgb model")
-    X_test = xgb.DMatrix(X_test)
+# if(model_path.find("xgb")):
+#     print("xgb model")
+#     X_test = xgb.DMatrix(X_test)
 
 y_pred = model.predict(X_test)
 
@@ -41,7 +41,7 @@ y_pred = model.predict(X_test)
 report = classification_report(y_test, y_pred)
 
 
-with open('./Reports/xgb_report.txt', 'w') as f:
+with open('./Reports/gnb_report.txt', 'w') as f:
     f.write(report)
 
-print("Classification report saved to 'xgb_report.txt'.")
+print("Classification report saved to 'gnb_report.txt'.")
